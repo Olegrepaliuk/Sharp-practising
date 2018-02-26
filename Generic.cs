c034a61bd0b9468e7c7e0d970d8fefc8c1e59fae
Oleg <Oleg58807@ukr.net>
2018-02-26 22:23:40 +0200

    Generic using

---
+++ Generic.cs
@@ -0,0 +1,39 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Document> dc = new List<Document>();
+            var t1 = Document.CreateDocument();
+            dc.Add(t1);
+            Console.WriteLine(t1);
+            Console.ReadKey();
+        }
+
+        public static T Max<T>(T[] source) where T: IComparable
+        {
+            if (source.Length == 0)
+            {
+                return default(T);
+            }
+            else
+            {
+                Array.Sort(source);
+                return source[source.Length - 1];
+            }
+        }
+    }
+    public class Document
+    {
+        public int Num { get; set; }
+        public string Name { get; set; }
+        public List<int> SomeNumbers { get; set; }
+        public static Document CreateDocument()
+        {
+            return new Document { Num = 9, Name = "Test" };
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Good document {0}, with number {1}", Name, Num);
+        }
+    }
