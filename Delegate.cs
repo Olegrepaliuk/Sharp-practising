7ff24d00811da3a3a08bf4ed8c13efdb5fa5299f
Oleg <Oleg58807@ukr.net>
2018-03-21 21:13:31 +0200

    Added lambda expressions

--- Delegate.cs
+++ Delegate.cs
@@ -34,4 +34,25 @@ namespace Delegate
 			return int.TryParse(answer, out age);
 		}
 	}
+
+
+	//Lambda expressions
+
+	private static readonly Func<int> zero = () => 0;
+	private static readonly Func<int, string> toString = (x) => Convert.ToString(x); 
+	private static readonly Func<double, double, double> add = (x,y) => x+y;
+	private static readonly Action<string, string> print = (str) => Console.WriteLine(str);
+
+
+	Func<int, int> doubleValue = x => 2 * x;
+	Func<int, int> minusOne = x => x - 1;
+	var f2 = Combine(minusOne, doubleValue);
+	Console.WriteLine(f2(2)); // 2
+	Console.WriteLine(f2(5)); // 8
+
+	static Func<T1, T3> Combine<T1, T2, T3>(Func<T1, T2> f, Func<T2, T3> g)
+	{
+		return x => g(f(x));
+	}
+
 }
